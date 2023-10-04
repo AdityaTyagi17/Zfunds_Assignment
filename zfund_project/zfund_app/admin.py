917f083677c98b3b5569c37e047889334b64411f
AdityaTyagi17 <125254793+AdityaTyagi17@users.noreply.github.com>
2023-10-04 16:28:29 +0530

    Update admin.py

--- zfund_project/zfund_app/admin.py
+++ zfund_project/zfund_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Product, ProductCategory  # Import the User model from your app's models
+from .models import User,Product, ProductCategory 
 
 # Register the User model
 admin.site.register(User)
